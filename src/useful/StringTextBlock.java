1958552889236b1289b0682bb6311c24f8c4de37
haejujung <ysin70@naver.com>
2024-04-26 10:36:18 +0900

    string buffer

---
+++ src/useful/StringTextBlock.java
@@ -0,0 +1,31 @@
+package useful;
+
+public class StringTextBlock {
+
+	public static void main(String[] args) {
+		
+		// """ 찍고 한줄 내리기
+		String strBlock = """
+					This 
+					is 
+					a 
+					Text 
+					block
+					""";
+		String a = """
+				ddddd
+				""";
+		String jsonText = """
+				{
+				"userId": 1,
+				  "id": 1,
+				  "title": "delectus aut autem",
+				  "completed": false
+				}
+  
+				""";
+		
+		// html,json 문자열을 만드는데 유용하게 사용할 수 있음
+	}
+
+}
